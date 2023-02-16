a64bbae49149c7f13720cdb2001802c483d9d982
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-02-16 05:28:04 -0800

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * return: Always 0(success)
- */
+*main -Entry point
+*Return: Always 0 (Success)
+*/
 
 int main(void)
 {
@@ -14,5 +13,5 @@ int main(void)
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
-
 }
+
