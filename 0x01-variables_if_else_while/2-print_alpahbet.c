1aa4c0172de285b0ad73fa6f31ffd07125a425a9
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-02-17 12:49:06 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alpahbet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ *
+ * Return: Return 0;
+ */
+
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar (lc);
+	}
+		putchar('\n');
+	return (0);
+}
