1fc54d87e1877de5b7a61582d82801ce0e0ae014
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-02-17 12:59:25 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets
+ * Return: Always 0;
+ */
+
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+	if (la != 'e' && la != 'q')
+		putchar (la);
+	}
+	putchar('\n');
+	return (0);
+}
