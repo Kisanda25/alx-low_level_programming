9ada3d872a9a7b1eb0fc6917fd67cce3dca315a7
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-02-17 17:41:19 -0800

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - combination of digits
+ * Return: always 0;
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar (n);
+	if (n != 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+		putchar('\n');
+	}
+	return (0);
+}
