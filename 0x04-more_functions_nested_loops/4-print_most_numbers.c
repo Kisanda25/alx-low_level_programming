e12441df62ae553d141f854f0a5a6f2ae0e715d9
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-02-23 09:59:18 -0800

    update

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print number between 0 and 9 except 2 and 4
+ * Return: always 0;
+ */
+
+void print_most_numbers(void)
+{
+	 int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50 || c != 52)
+		{
+		_putchar(c);
+	}
+	}
+	_putchar('\n');
+}
