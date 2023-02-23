6e33544b686237f17b25060ea2e322df00febca2
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-02-23 08:44:24 -0800

    update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: always 0 ;
+ */
+
+void print_numbers(void)
+{	
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
