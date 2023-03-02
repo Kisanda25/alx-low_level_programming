cf141191a617ef3b8fa144f4218b676443aea2bd
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-03-02 07:48:37 -0800

    update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,23 @@
 #include <main.h>
 
 /**
-* _strcat - used to concat two strings 
+* _strcat - used to concat two strings
 * @dest: the first string to be used
 * @src: the second string to be used
 * Return: we return dest
 */
 
-
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+	int j;
+
 	for (i = 0; dest[i] != '\0'; i++)
+
+	for (j = 0; src[j] != '\0'; j++)
 	{
+	dest[i + j] = src[j];
 	}
-	for (j = 0; src[j] != '\0'; j++) {
-	dest[i+j] = src[j];
-	}
-	dest[i+j] = '\0';
-	return dest;
+	dest[i + j] = '\0';
+	return (dest);
 }
