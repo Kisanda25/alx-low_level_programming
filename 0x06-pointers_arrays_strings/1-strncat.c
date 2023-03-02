a93f7f8cf78b7a1aa646fced1fae67f072773b7b
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-03-02 08:27:20 -0800

    update

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strncat - concats until n
+* @dest: the first var
+* @src: the seconf var
+* @n: the third var
+* Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0' || j <= n)
+	{
+		i++;
+		j++
+	}
+
+	return (dest);
+
+}
