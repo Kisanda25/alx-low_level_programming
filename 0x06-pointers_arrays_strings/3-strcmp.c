ef2eb5680c79dfebfd0cf3b087f64d719c431119
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-03-02 12:46:46 -0800

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 > s2)
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		return (15);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
-	if (s2 > s1)
-	{
-		return (-15);
-	}
-	else
+
 		return (0);
 }
