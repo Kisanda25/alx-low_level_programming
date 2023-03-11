1aa2e6283953af298f03cd4db3162a5b353f0f87
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-03-10 17:08:34 -0800

    args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,8 +2,6 @@
 
 #include "main.h"
 
-
-
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
@@ -29,7 +27,6 @@ int _atoi(char *s)
 	{
 		if (s[i] == '-')
 			++d;
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -44,7 +41,38 @@ int _atoi(char *s)
 		i++;
 	}
 	if (f == 0)
-		return (0);
+	return (0);
+
 	return (n);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = _atoi(argv[1]);
+
+	num2 = _atoi(argv[2]);
+
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
 }
 
