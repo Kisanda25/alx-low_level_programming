707115bce9efd0a49c0f4778c5d6bceb58289875
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-03-20 08:52:20 -0700

    a header dog.h is created which creates a struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * struct dog - creates a struct with 3 attributes
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
 struct dog
 {
 	char *name;
 	float age;
-	char *owner;	
+	char *owner;
 };
 
 #endif
