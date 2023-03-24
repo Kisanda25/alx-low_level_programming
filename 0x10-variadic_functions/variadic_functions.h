96a851ff2cf3a08fd834bef04f49c0efeda1c51a
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-03-23 18:30:00 -0700

    variadic function

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
 
-#include <stddef.h>
-#include <stdlib.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
 #endif
