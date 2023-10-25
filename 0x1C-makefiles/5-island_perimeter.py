f7d75d7f52a40ae92b6ed5057be57edceb8c22f2
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-10-25 11:57:38 -0700

    C - Makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a function that returns the perimeter of the island described in grid"""
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in the grid.
+
+    Parameters:
+    grid (List[List[int]]): A list of lists of integers
+
+    Returns:
+    int: The perimeter of the island.
+    """
+    if not grid:
+        return 0
+
+    perimeter = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[0]):
+            if grid[row][col] == 1:
+                perimeter += 4
+                # Check adjacent cells (up, down, left, right)
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
